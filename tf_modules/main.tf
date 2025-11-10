18ae8185aa78fb383ac6b233eae08c41de4e22a2
Nikhil <nikhilreddvch2012@gmail.com>
2025-11-10 10:34:17 +0530

    fix terraform code

--- tf_modules/main.tf
+++ tf_modules/main.tf
@@ -7,11 +7,6 @@ tenant_id       = var.tenant_id
 subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
@@ -34,29 +29,35 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
 resource "azurerm_virtual_machine" "vm" {
-  name                = "${var.prefix}-vm"
-  resource_group_name = data.azurerm_resource_group.resource_group.name
-  location            = var.location
-  vm_size                = var.vm_size
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
+  name                  = "${var.prefix}-vm"
+  resource_group_name   = data.azurerm_resource_group.resource_group.name
+  location              = var.location
+  vm_size               = var.vm_size
+  network_interface_ids = [azurerm_network_interface.nic.id]
   delete_os_disk_on_termination = true
 
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
   storage_os_disk {
-    name = "AzureAMIImage"
-    caching              = "ReadWrite"
-    create_option = "fromImage"
+    name              = "AzureAMIImage"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
-os_profile {
-  computer_name = "ubuntu-server"
-  admin_username = var.admin_username
-}
+  os_profile {
+    computer_name  = "ubuntu-server"
+    admin_username = var.admin_username
+  }
 
-os_profile_linux_config {
-  disable_password_authentication = true
+  os_profile_linux_config {
+    disable_password_authentication = true
   }
 }
