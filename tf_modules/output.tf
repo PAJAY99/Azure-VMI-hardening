cd0d78ae215c8cc9e73f02fb7a088c38bc194136
Nikhil <nikhilreddvch2012@gmail.com>
2025-11-18 15:05:53 +0530

    add outputs

--- tf_modules/output.tf
+++ tf_modules/output.tf
@@ -1,4 +1,7 @@
-output "instance_ip" {
-  description = "Public IP of the VM"
-  value       = azurerm_public_ip.public_ip.ip_address
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
 }
